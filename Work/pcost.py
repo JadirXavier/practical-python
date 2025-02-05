f0459d9dddf9afb6a3e39c59dfdca2013c9d309f
Jadir Xavier <49170436+JadirXavier@users.noreply.github.com>
2025-02-05 18:37:37 +0000

    1.7

--- Work/pcost.py
+++ Work/pcost.py
@@ -1,3 +1,20 @@
 # pcost.py
 #
 # Exercise 1.27
+
+import os
+
+f = open('./Data/portfolio.csv','rt')
+next(f)
+
+total = 0
+
+for line in f:
+    row = line.replace('\n','').split(',')
+    price = float(row[-1])*float(row[-2])
+    print(price)
+    total += price
+
+print(f'Total cost ${total:0.2f}')
+
+f.close()
